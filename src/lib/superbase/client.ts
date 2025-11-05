2fca87e163f6e33cf19073ccdffb8e29604925d5
himanshumannu51-collab <himanshu.mannu51@gmail.com>
2025-11-06 03:04:50 +0530

    Update client.ts

--- src/lib/superbase/client.ts
+++ src/lib/superbase/client.ts
@@ -1,8 +1,7 @@
-import { createBrowserClient } from '@supabase/ssr'
+// lib/supabase.ts
+import { createClient } from "@supabase/supabase-js";
 
-export function createClient() {
-  return createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-  )
-}
+export const supabase = createClient(
+  process.env.SUPABASE_URL!,
+  process.env.SUPABASE_ANON_KEY!
+);
